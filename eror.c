fe5356b3c8a2deda9b9305130f0fdb5a8e12519e
Shubham Sharma <shubham@Shubhams-MacBook-Air.local>
2026-02-02 20:57:09 +0530

    hello

---
+++ eror.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+// int main()
+// {
+//     printf(5 + "ABCDEQuiz");
+//     return 0;
+
+// }
+
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter your number:" );
+    scanf("%d",&x);
+    printf("Your number is %d",x);
+
+    return 0;
+
+}
