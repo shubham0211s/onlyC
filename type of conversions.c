f924bea11ed0c761aba6953b2cdd387a35427b85
Shubham Sharma <shubham@Shubhams-MacBook-Air.local>
2026-02-02 20:19:45 +0530

    Added type of conversions example

---
+++ type of conversions.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int x = 10;
+//     char y = 'a';
+//     x = x+y;
+
+//     float z = x+1.0;
+//     printf("x = %d,z = %f",x,z);
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    double x = 1.2;
+    int sum = (double)x +1;
+    printf("sum= %d",sum);
+    return 0;
+}
