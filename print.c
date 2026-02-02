533d864b8789eef95472ec45773d5eed051c61b9
Shubham Sharma <shubham@Shubhams-MacBook-Air.local>
2026-02-02 20:38:07 +0530

    important

---
+++ print.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+
+    int b;
+    scanf("%d",&b);
+
+    double c;
+    scanf("%lf",&c);
+
+    float d;
+    scanf("%f",&d);
+
+    printf("The enter character: %c\n",a);
+    printf("The enter Integer: %d\n",b);
+    printf("The enter Double: %lf\n",c);
+    printf("The enter float: %f\n",d);
+
+
+}
