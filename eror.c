208f7d7aa7bc58789363d4ced12e2fbc87d0a834
Shubham Sharma <shubham@Shubhams-MacBook-Air.local>
2026-02-02 21:12:56 +0530

    Updated files

--- eror.c
+++ eror.c
@@ -18,14 +18,33 @@
 
 // }
 
+// #include <stdio.h>
+// int main()
+// {
+//     char x[5];
+//     printf("Enter your number:" );
+//     scanf("%4s",x);
+//     printf("Your number is %s",x);
+
+//     return 0;
+
+// }
+
 #include <stdio.h>
+
 int main()
 {
-    char x[5];
-    printf("Enter your number:" );
-    scanf("%4s",x);
-    printf("Your number is %s",x);
+    int a, b;
 
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("\nResults:\n");
+    printf("Addition: %d\n", a + b);
+    printf("Subtraction: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
     return 0;
-
 }
